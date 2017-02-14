4bde1836be011ee039caadf08d629866e04ec112
Paul <paucho1437@yahoo.com>
2017-02-14 10:36:08 -0600

    ruby assignment 6 menu corrected

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     # #3
@@ -40,6 +41,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system "clear"
+        entry_n_submenu
+        main_menu
+      when 6
         puts "Good-bye!"
         # #8
         exit(0)
@@ -50,6 +55,21 @@ class MenuController
         main_menu
       end
     end
+
+   def entry_n_submenu
+     print "Entry number to view: "
+     selection = gets.chomp.to_i
+
+     if selection < address_book.entries.count
+       puts address_book.entries[selection]
+       print "Press Enter to return to main menu"
+       gets.chomp
+       system "clear"
+     else
+       puts "#{selection} is invalid."
+       entry_n_submenu
+     end
+   end
 
    def view_all_entries
      # #14

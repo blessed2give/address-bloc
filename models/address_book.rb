a249f70ad7d617439115f0519411b7d8c88280e1
Paul <paucho1437@yahoo.com>
2017-02-15 16:59:00 -0600

    ruby cp 8 searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -32,6 +32,31 @@ class AddressBook
       end
     end
 
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # #5
+    return nil
+  end
+
   def remove_entry(name, phone_number, email)
     delete_entry = nil
     entries.each do |entry|
